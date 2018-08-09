f0e48324564e845e64bd75ef0f0d57787a24e330
Florence Fong <florence.r.fong@gmail.com>
2018-08-09 16:13:20 -0400

    wip, troubleshooting carousel src images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import TheNavbar from './TheNavbar';
+import TheCarousel from './TheCarousel';
 import './App.css';
 
 class App extends Component {
@@ -8,7 +9,10 @@ class App extends Component {
   render() {
 
     return (
+      <div>
       <TheNavbar />
+      <TheCarousel />
+      </div>
     );
   }
 }
