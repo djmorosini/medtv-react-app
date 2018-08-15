69e1f7ab4f301512f2a8f6985794f958e5e098d3
djmorosini <djmorosini@outlook.com>
2018-08-15 08:53:37 -0400

    added /'s to paths for static pages

--- src/TheFooter.js
+++ src/TheFooter.js
@@ -7,8 +7,8 @@ export default class TheFooter extends React.Component {
         return (
             <div width='100%' className="py-5 bg-primary footer">
                 <div className="container">
-                    <p className="m-0 text-center text-white">Copyright &copy; MedTV 2018 || <a className="text-white" href="about.html">About</a> ||
-                    <a className="text-white" href="contact.html"> Contact</a></p>
+                    <p className="m-0 text-center text-white">Copyright &copy; MedTV 2018 || <a className="text-white" href="/about.html">About</a> ||
+                    <a className="text-white" href="/contact.html"> Contact</a></p>
                 </div>
             </div>
         )
