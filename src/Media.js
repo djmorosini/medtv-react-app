375f7cdaa2b7203014632b314c7d687aedb72a1e
JuliaKan <39783445+JuliaKan@users.noreply.github.com>
2018-08-16 15:04:13 -0400

    added photos to Our team

--- src/Media.js
+++ src/Media.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Media } from 'reactstrap';
-import { AgentSmith } from './images/AgentSmith.jpg'
+import AgentSmith from './images/AgentSmith.jpg';
+import Dozer from './images/Dozer.jpg';
+import TheOracle from './images/TheOracle.jpg';
+import Trinity from './images/Trinity.jpg';
+import Morpheus from './images/Morpheus.jpg';
+import Neo from './images/Neo.jpg'
+
 
 
 
@@ -9,36 +15,71 @@ const Team = () => {
     <div>
       <Media>
         <Media left top href="#">
-          <Media object data-src="https://placebeyonce.com/420-320" alt="Generic placeholder image" />
+          <Media object src={ AgentSmith } alt="Picture of" />
         </Media>
         <Media body>
           <Media heading>
-            Top aligned media
+            Mat Gilbert
           </Media>
-          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-        </Media>
+            Mat is our TA.
+            </Media>
       </Media>
       <Media className="mt-1">
         <Media left middle href="#">
-        <Media object src={ AgentSmith } alt="Chris is Agent Smith"/>
+        <Media object src={ AgentSmith } height="200px" alt="Picture of"/>
         </Media>
         <Media body>
           <Media heading>
-            Middle aligned media
-          </Media>
-          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-        </Media>
+             Florence Fong
+            </Media>
+            Flo is ...
+            </Media>
       </Media>
       <Media className="mt-1">
         <Media left bottom href="#">
-          <Media object data-src="holder.js/64x64" alt="Generic placeholder image" />
+          <Media object src={ Neo } alt="Picture of" />
+        </Media>
+        <Media body>
+            <Media heading>
+                Dylan Morosini
+                </Media>
+                Dylan can't stop won't stop.
+                </Media>
+      </Media>
+      <Media>
+        <Media left top href="#">
+          <Media object src={ Dozer } alt="Picture of" />
         </Media>
         <Media body>
           <Media heading>
-            Bottom aligned media
+            Matt Layden
           </Media>
-          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+            Mat is ...
+            </Media>
+      </Media>
+      
+      <Media className="mt-1">
+        <Media left middle href="#">
+        <Media object src={ Trinity } alt="Picture of"/>
         </Media>
+        <Media body>
+          <Media heading>
+             Julia Kantarovsky
+            </Media>
+            Julia is ...
+            </Media>
+      </Media>
+     
+      <Media className="mt-1">
+        <Media left middle href="#">
+        <Media object src={ Morpheus } alt="Picture of"/>
+        </Media>
+        <Media body>
+          <Media heading>
+             Chris Frank
+            </Media>
+            Chris loves the Agile.
+            </Media>
       </Media>
     </div>
   );
