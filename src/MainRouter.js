dd11a757656f2c09d44d2ef36aa68050a076e2d1
JuliaKan <39783445+JuliaKan@users.noreply.github.com>
2018-08-16 14:18:46 -0400

    added our team photos to directory and Meet our team page

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -2,7 +2,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import React from 'react';
 import Homepage from './Homepage';
 import FullListPage from './FullListPage';
-import VideoWatchPage from './VideoWatchPage'
+import VideoWatchPage from './VideoWatchPage';
+import MeetOurTeam from './MeetOurTeam';
 
 const Main = () => (
   <BrowserRouter>
@@ -10,8 +11,9 @@ const Main = () => (
       <Route name='home' exact path='/' component={Homepage}/>
       <Route name='watch-page' exact path='/video/:id' component={VideoWatchPage}/>
       <Route name='list-page' path='/videos' component={FullListPage}/>
+      <Route name='meet-our-team' path='/meet-our-team' component={MeetOurTeam}/>
     </Switch>
   </BrowserRouter>
 )
 
-export default Main
+export default Main;
