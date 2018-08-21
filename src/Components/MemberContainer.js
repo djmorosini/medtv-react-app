43bc618938f547196a0ddc4e7e0a10ba2f04acc4
JuliaKan <39783445+JuliaKan@users.noreply.github.com>
2018-08-21 14:58:41 -0400

    styling for member details page and bio

--- src/Components/MemberContainer.js
+++ src/Components/MemberContainer.js
@@ -3,9 +3,10 @@ import { Container, Row, Col } from 'reactstrap';
 import AgentSmith from '../images/AgentSmith.jpg';
 import Dozer from '../images/Dozer.jpg';
 import TheOracle from '../images/TheOracle.jpg';
-import Trinity from '../images/Trinity1.jpg';
+// import Trinity from '../images/Trinity1.jpg';
 import Morpheus from '../images/Morpheus1.jpg';
-import Neo from '../images/Neo1.jpg'
+import Neo from '../images/Neo1.jpg';
+import Julia from '../images/julia400px.jpg'
 
 
 
@@ -26,6 +27,7 @@ export default class Example extends React.Component {
               <a href="https://www.linkedin.com/in/florence-fong/">LinkedIn</a>
               <br />
               <a href="https://github.com/flofong">GitHub</a>
+              <p className="whatIDid">Flo has worked a lot on this project</p>
             </Col>
           </Row>
 
@@ -45,6 +47,7 @@ export default class Example extends React.Component {
               <a href="https://www.linkedin.com/in/matthew-layden-68059a167/">LinkedIn</a>
               <br />
               <a href="https://github.com/kobello">GitHub</a>
+              <p className="whatIDid">Matt has worked a lot on this project</p>
             </Col>
           </Row>
 
@@ -64,6 +67,7 @@ export default class Example extends React.Component {
               <a href="https://www.linkedin.com/in/djmorosini/">LinkedIn</a>
               <br />
               <a href="https://github.com/djmorosini">GitHub</a>
+              <p className="whatIDid">Dylan has worked a lot on this project</p>
             </Col>
           </Row>
 
@@ -76,13 +80,14 @@ export default class Example extends React.Component {
 
           <Row>
             <Col xs="6 imageHolder">
-              <img className="matrixPic" src={Trinity}></img>
+              <img className="matrixPic" src={Julia}></img>
             </Col>
-            <Col xs="6">
+            <Col xs="6 information">
               <h1>Julia Kantarovsky</h1>
               <a href="https://linkedin.com/in/juliakantarovsky">LinkedIn</a>
               <br />
               <a href="https://github.com/JuliaKan">GitHub</a>
+              <p className="whatIDid">Julia has worked a lot on this project</p>
             </Col>
           </Row>
 
@@ -102,6 +107,7 @@ export default class Example extends React.Component {
               <a href="https://www.linkedin.com/in/christopherfrank/">LinkedIn</a>
               <br />
               <a href="https://github.com/cfrank1977">GitHub</a>
+              <p className="whatIDid">Chris has worked a lot on this project</p>
             </Col>
           </Row>
 
@@ -121,6 +127,7 @@ export default class Example extends React.Component {
               <a href="Mat/linkedin">LinkedIn</a>
               <br />
               <a href="Mat/github">GitHub</a>
+              <p className="whatIDid">Mat was the advisor on this project</p>
             </Col>
           </Row>
 
